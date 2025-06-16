809a767059db067fa40dc74f873cf3ac0f21acae
Ryujin <w230037@e.ntu.edu.sg>
2025-06-16 20:51:29 +0700

    fix: remove MDXContentFilter export from index.ts

--- apps/docs/src/components/index.ts
+++ apps/docs/src/components/index.ts
@@ -20,5 +20,4 @@ export {
   DynamicFormControlsShowcase, 
   DynamicAvatarShowcase 
 } from './DynamicShowcases';
-export { MDXContentFilter } from './MDXContentFilter';
 export { MDXWrapper } from './MDXWrapper';
